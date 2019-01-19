7278cb229e89cd77f3827c287a770daba15228c7
Risto Välimäki <risto.valimaki@wdata.fi>
2019-01-19 03:12:10 +0200

    Ensimmäinen versio, pistepörssi toimii demona

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'turnaus';
+export class AppComponent implements OnInit {
+
+
+  constructor(private db: AngularFireDatabase) {
+  }
+
+  teams: any[] = [];
+  games: any[] = [];
+  players: any[] = [];
+
+  private playerSubscription: Subscription;
+  private teamSubscription: Subscription;
+  private gameSubscription: Subscription;
+
+
+  ngOnInit() {
+    this.gameSubscription = this.db.list('games').valueChanges().subscribe(
+      games => {
+        this.games = games;
+      });
+
+    this.playerSubscription = this.db.list('players').valueChanges().subscribe(
+      players => {
+        this.players = players;
+
+        this.sortPlayers();
+
+      }
+    );
+
+    this.teamSubscription = this.db.list('teams').valueChanges().subscribe(
+      teams => {
+        this.teams = teams;
+
+      }
+    );
+  }
+
+  private sortPlayers() {
+    this.players.sort((a, b) => {
+      if(b.points == null) return -1;
+      if(a.points == null) return 1;
+
+      if(a.points > b.points) return -1;
+      if(a.points < b.points) return 1;
+
+      if(a.goals > b.goals) return -1;
+      if(a.goals < b.goals) return 1;
+
+      if(a.primaryAssists > b.primaryAssists) return -1;
+      if(a.primaryAssists < b.primaryAssists) return 1;
+
+      if(a.penaltyMinutes > b.penaltyMinutes) return 1;
+      if(a.penaltyMinutes < b.penaltyMinutes) return -1;
+
+      if(a.number > b.number) return 1;
+      if(a.number < b.number) return -1;
+
+      if(a.team > b.team) return 1;
+      if(a.team < b.team) return -1;
+
+      return 0;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { firebaseConfig } from './firebaseConfig';
 
 @NgModule({
   declarations: [
@@ -10,9 +22,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
