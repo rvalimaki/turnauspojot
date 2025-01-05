8324dd985116dcc9a420744659b1d0b985344357
Risto Välimäki <risto.valimaki@gmail.com>
2025-01-05 17:14:23 +0200

    Fix settings, logos and logo placement

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,18 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { TitleService } from '../title.service';
+import { Component, OnInit } from "@angular/core";
+import { TitleService } from "../title.service";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.scss"],
 })
 export class DashboardComponent implements OnInit {
-
   constructor(private titleService: TitleService) {
-    this.titleService.title = 'Sonkajärvi I — 3.10.2020';
+    this.titleService.title = "Urjala-turnaus X — 11.1.2025";
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
